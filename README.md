a569dbbf60e0c46a313f41bc4c1d92b65f690bf3
annde1 <annadepta61@gmail.com>
2023-09-26 10:23:00 +0300

    First push

--- README.md
+++ README.md
@@ -1,70 +1,44 @@
-# Getting Started with Create React App
+# React Pizza Co.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![React Pizza Co.](link_to_project_screenshot_or_logo.png)
 
-## Available Scripts
+React Pizza Co. is a simple web application that showcases a menu of delicious Italian pizzas. This project is built using React and demonstrates the use of React components, conditional rendering, and more.
 
-In the project directory, you can run:
+## Table of Contents
 
-### `npm start`
+- [Demo](#demo)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Demo
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+You can see a live demo of the project [here](link_to_demo).
 
-### `npm test`
+![Demo](link_to_demo_screenshot_or_gif.gif)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Features
 
-### `npm run build`
+- Display a menu of Italian pizzas.
+- Conditional rendering based on pizza availability.
+- Show opening hours and the option to place an order.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Getting Started
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Prerequisites
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Before you begin, ensure you have the following dependencies installed:
 
-### `npm run eject`
+- Node.js: [Download](https://nodejs.org/)
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+### Installation
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+1. Clone the repository:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+```bash
+git clone https://github.com/yourusername/react-pizza-co.git
+```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,164 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+/*Importing external css file: */
+import "./index.css";
+//Profile Card Challange: "https://codesandbox.io/s/crimson-browser-pqry3v?file=/src/index.js"
+// https://codesandbox.io/s/react-challenge-dev-profile-final-v2-e8zj9o?file=/src/index.js
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Nesting:
+const App = () => {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Menu></Menu>
+      <Footer></Footer>
+    </div>
+  );
+};
+const Header = () => {
+  //Css properties with "-" were converted in React to camelCase: text-align -> textAlign
+  //   const style = {}; //We can pass style object
+  return (
+    <header className="header">
+      <h1>React Pizza Co.</h1>
+    </header>
+  );
+};
+const Menu = () => {
+  //I guess that here we will map on the pizzas array and return Pizza component with props
+  //Passing the props in the component:
+  //Pasing the current Pizza Object
+  const pizzas = pizzaData; //Conditional rendering. Render the list of pizzas only if there are some pizzas
+  const numOfPizzas = pizzas.length;
+  return (
+    //we need to check for length of pizzas array, if it's grater than 0. We can't use just a number because if zero will be returned then 0 will be render (that's how short circuting works)
+    <main className="menu">
+      <h2>Our Menu</h2>
+
+      {numOfPizzas > 0 ? (
+        <Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from out stone oven,a ll organic , all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza, index) => (
+              <Pizza key={index} pizzaObj={pizza}>
+                {" "}
+              </Pizza>
+            ))}
+          </ul>
+        </Fragment>
+      ) : (
+        <p>We're still working on out menu. Please come back later.</p>
+      )}
+    </main>
+  );
+};
+// {} accepts anything that produces value. Noo if else
+//Passing the props in the component (as function argument) using destructuring. the destructured object needs to exctly match the prop obj name
+const Pizza = ({ pizzaObj }) => {
+  console.log(pizzaObj);
+  //If pizza is sold out return early
+  //   if (pizzaObj.soldOut) return null;
+  //If we return early this won't be rendered
+  //Passing className conditionally using ternary operator
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+        <span></span>
+      </div>
+    </li>
+  );
+};
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 14;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //Possible to do conditional rendering with multiple returns:
+  //   if (!isOpen) {
+  //     return <p>CLOSED</p>;
+  //   }
+  return (
+    <footer className="footer">
+      {" "}
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour}></Order>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour} and {closeHour}
+        </p>
+      )}
+    </footer>
+  );
+  //we're currently open will be rendered or not based on the value returned from isOpen (short circuting). In && operator two values must be true
+};
+//Extarted the order into it's own component. Passing theclose hour as props using destructurig
+const Order = ({ closeHour, openHour }) => {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+};
+
+//React version 18 (rendering root):
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+); //Root component renders the App
+//Before React 18:
+//React.render(<App />,document.getElementById("root") )
